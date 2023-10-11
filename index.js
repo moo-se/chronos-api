e500022d0e37e3c22482e29b7bf267f11faf5c3e
moo-se <omedeomozuwa@gmail.com>
2023-10-11 18:29:57 +0100

    vanilla node.js vs express.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 3001
+
+const server = http.createServer(async(req,res)=>{
+    res.write('<h1>Hello, World!</h1>')
+    res.statusCode=200
+    res.end()
+})
+
+server.listen(PORT, ()=>{
+    console.log(`server is listening on port ${PORT}`)
+})
